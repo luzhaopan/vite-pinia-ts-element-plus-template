5da9f04b8f800d1bcc3d2630ede4eb1ea3173c36
luzhaopan <745003249@qq.com>
2023-08-22 15:29:31 +0800

    fix port

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // 配置前端服务地址和端口
+	server: {
+    host: '0.0.0.0',//自定义主机名
+    port: 8000,//自定义端口
+    // 是否开启 https
+    https: false,
+  },
+  // 设置反向代理，跨域
+  // proxy: {
+  // 	'/api': {
+  // 		// 后台地址
+  // 		target: 'http://127.0.0.1:8000/',
+  // 		changeOrigin: true,
+  // 		rewrite: path => path.replace(/^\/api1/, '')
+  // 	}
 })
