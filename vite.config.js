cd4649e414e9137f8043c6248603603ed485558a
luzhaopan <745003249@qq.com>
2023-10-19 15:20:41 +0800

    fix: install

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0', //自定义主机名
+    port: '8000', // 自定义端口
+    open: true, // 自动在浏览器打开
+    https: false, // 是否开启 https
+    // 设置反向代理，跨域
+    proxy: {
+      '/api': {
+        // 后台地址
+        target: 'http://127.0.0.1:8000/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
