607d6d8fdb154fbc3ee8147a92e7843982c67295
luzhaopan <745003249@qq.com>
2023-10-23 10:49:22 +0800

    fix: eslint index 驼峰警告问题

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,14 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+     //在rules中添加自定义规则
+     "vue/multi-word-component-names":"off" //关闭组件命名规则
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
